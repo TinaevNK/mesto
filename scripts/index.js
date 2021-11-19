483e0dda89611fae497a060d9d28b0c5b3c54cd3
Nikita Tinaev <Nikkach1997@yandex.ru>
2021-11-20 01:51:04 +0300

    feat: напиши код для рендеринга карточек

--- scripts/index.js
+++ scripts/index.js
@@ -2,21 +2,23 @@
 
 // ищем в документе наш попап
 const popupElement = document.querySelector('.popup');
+
 // кнопка закрытия попапа
 const closeButton = document.querySelector('.popup__close-button');
 // кнопка "редактировать профиль"
 const editButton = document.querySelector('.profile__edit-button');
+
 // ищем нашу форму
 const formElement = document.querySelector('.popup__form');
 // её поле c именем
 const nameInput = formElement.querySelector('#name');
 // её полен с работой
 const jobInput = formElement.querySelector('#job');
+
 // ищем в документе <input> с именем
 const nameProfile = document.querySelector('.profile__name');
 // ищем в документе <input> с работой
 const jobProfile = document.querySelector('.profile__job');
-
 
 // объявляем функцию открытия поп-ап,а и добавляем модификатор
 function openPopup() {
@@ -60,3 +62,57 @@ closeButton.addEventListener('click', closePopup);
 // обработчик события отправки данных на сервер, в нашем случае при нажатии
 // кнопки "сохранить", т.к. её type=submit
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+// рендеринг карточек
+
+// первоначальный массив карточек для template
+const initialCards = [
+  {
+    name: 'Мачу-Пикчу',
+    link: './images/machu-piсchu.jpg'
+  },
+  {
+    name: 'Озеро Морейн',
+    link: './images/moraine-lake.jpg'
+  },
+  {
+    name: 'Памуккале',
+    link: './images/pamukkale.jpg'
+  },
+  {
+    name: 'Скафтафетль',
+    link: './images/skaftafell.jpg'
+  },
+  {
+    name: 'Снайфедльсйёкюдль',
+    link: './images/svínafellsjökull.jpg'
+  },
+  {
+    name: 'Териберка',
+    link: './images/teriberka.jpg'
+  }
+];
+
+// находим шаблон
+const cardTemplate = document.querySelector('#cardTemplate');
+// находим контейнер, куда будем вставлять клоны шаблона
+const cardContainer = document.querySelector('.elements__list');
+
+// функция рендеринга карточек
+const createCardDomNode = (card) => {
+  const cardTemplateElement = cardTemplate.content.querySelector('.element').cloneNode(true); //клонируем
+  cardTemplateElement.querySelector('.element__title').textContent = card.name; // заполняем контентом
+  cardTemplateElement.querySelector('.element__photo').src = card.link;
+  cardTemplateElement.querySelector('.element__photo').alt = card.name;
+
+  return cardTemplateElement;
+}
+
+// функция добавления карточек
+const addCard = initialCards.map((card) => { // перебираем массив и создаём новый
+  return createCardDomNode(card); // передаём элементы массива функции и вызываем её
+});
+
+// добавление в разметку карточек
+cardContainer.append(...addCard);
